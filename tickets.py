6d91bb55455d1b1255f646fc25e1ba9dbdc25910
ShabrovaSveta <svetashabrova@gmail.com>
2022-09-15 22:21:18 +0400

    Add files via upload

---
+++ tickets.py
@@ -0,0 +1,34 @@
+tickets = dict()
+ticket_count = int(input("Введите кол-во билетов: "))
+
+for i in range(ticket_count):
+    age = int(input(f"Введите возраст для билета {i+1}: "))
+    if age < 18:
+        tickets[i] = 0
+    elif 18 <= age < 25:
+        tickets[i] = 990
+    elif age >= 25:
+        tickets[i] = 1390
+
+summa = sum(tickets.values())
+skidka = summa * 0.1
+print("Сумма:", summa)
+
+if ticket_count > 3:
+    print("Сумма со скидкой 10%:", summa - skidka)
+
+"""
+1. range(ticket_count) для например ticket_count = 3 вернёт в цикле 0, 1, 2
+2. сохраняем цены в словарь tickets.
+3. словарь возвращает список ключей методом keys(), а список значений методом values()
+Ключи словаря будут номер билета. Он не нужен, а нужны значения для суммирования. 
+Чтобы получить сумму значений получим их через values()
+и суммируем методом sum()
+4. надеюсь правильно считает процент скидки :)
+
+Строки с переменными смешивать можно разными способами:
+name = "Petya"
+1) "Hello, " + name
+2) f"Hello, {name}"
+3) "Hello, {the_name}".format(the_name=name)
+"""
